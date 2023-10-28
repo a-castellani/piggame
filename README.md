0e2b769a6e57ed42ab45bf45b54850bd17177e58
Andres Castellani <andresadc@gmail.com>
2023-10-28 20:54:28 +0200

    commit all the files

---
+++ README.md
@@ -0,0 +1,5 @@
+Online version of the famous Pig game.
+
+Made it after followed the course The complete JavaScript course: from Zero to Expert!(Udemy) with Jonas Schmedtmann.
+
+Using JavaScript, CSS and HTML.
---
+++ script.js
@@ -0,0 +1,102 @@
+'use strict';
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnRules = document.querySelector('.btn--rules');
+const displayRules = document.querySelector('.rules');
+const displayOverlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.btn__close');
+
+let currentScore, activePlayer, score, playing;
+
+const init = function () {
+  // Stating conditions
+  currentScore = 0;
+  activePlayer = 0;
+  score = [0, 0];
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player1El.classList.remove('player--active');
+  player0El.classList.add('player--active');
+};
+
+init();
+const switchPlayer = function () {
+  currentScore = 0;
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    // 1. Generate a random number
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Game and switch player logic
+    if (dice !== 1) {
+      currentScore += dice;
+      document.querySelector(`#current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // 1. Hold score
+    score[activePlayer] += currentScore;
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      score[activePlayer];
+    // 2. Comparing score lower or equal to 100
+    if (score[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+// New game button
+btnNew.addEventListener('click', init);
+
+// Rules open and close
+btnRules.addEventListener('click', function () {
+  displayRules.classList.remove('no_display_rules');
+  displayOverlay.classList.remove('no_display_rules');
+});
+
+btnClose.addEventListener('click', function () {
+  displayRules.classList.add('no_display_rules');
+  displayOverlay.classList.add('no_display_rules');
+});
